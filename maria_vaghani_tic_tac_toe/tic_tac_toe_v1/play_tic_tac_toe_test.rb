7e20ed41ac62832908f1192ff73b37e7a8671d21
mariavaghani <maria.vaghani@gmail.com>
2021-08-18 03:12:13 -0400

    implement board class for tic tac toe game

---
+++ maria_vaghani_tic_tac_toe/tic_tac_toe_v1/play_tic_tac_toe_test.rb
@@ -0,0 +1,165 @@
+require_relative "board"
+
+board = Board.new()
+
+position_arr = [1, 1]
+
+
+begin
+  board.place_mark([-1, 2], :X)
+rescue
+  puts "Place mark was not successful, as it should - GREAT"
+end
+
+begin
+  board.place_mark([0, 1], :X)
+  board.place_mark([1, 0], :O)
+  puts "Mark put was successful, as it should - GREAT"
+rescue 
+  puts "Mark put was not successful, FAIL"
+end
+
+begin
+  board.print
+  puts "Print method works - GREAT"
+rescue
+  puts "Print method was not successful, FAIL"
+end
+
+begin
+  board = Board.new()
+  board.place_mark([0, 1], :X)
+  board.place_mark([1, 0], :X)
+  board.place_mark([1, 1], :X)
+  p board.win_row?(:X)
+  # board.print
+  board.place_mark([1, 2], :X)
+  p board.win_row?(:X)
+  # board.print
+  puts "win_row method works GREAT"
+rescue
+  puts "win_row method was not successful, FAIL"
+end
+
+
+begin
+  board = Board.new()
+  board.place_mark([0, 1], :X)
+  board.place_mark([1, 1], :X)
+  board.place_mark([0, 2], :X)
+  if board.win_col?(:X) != false
+    raise "win_col method was not successful"
+  end
+  puts "passed win_row false - GREAT"
+  # board.print
+  board.place_mark([2, 1], :X)
+  if board.win_col?(:X) != true
+    raise "win_col method was not successful"
+  end
+  # board.print
+
+  puts "win_col method works GREAT"
+rescue
+  puts "win_col method was not successful, FAIL"
+end
+
+# #win_diagonal?
+
+begin
+  board = Board.new()
+  board.place_mark([0, 0], :X)
+  board.place_mark([1, 1], :X)
+  board.place_mark([0, 2], :X)
+  # board.print
+  if board.win_diagonal?(:X) != false
+    raise "win_diagonal method was not successful"
+  end
+  puts "passed win_diagonal false - GREAT"
+  board.place_mark([2, 2], :X)
+  # board.print
+  if board.win_diagonal?(:X) != true
+    raise "win_diagonal method was not successful"
+  end
+
+  puts "win_diagonal method works for diagonal 1 GREAT"
+rescue
+  puts "win_diagonal method was not successful, FAIL"
+end
+
+begin
+  board = Board.new()
+  board.place_mark([0, 0], :X)
+  board.place_mark([0, 2], :X)
+  board.place_mark([1, 1], :X)
+  # board.print
+  if board.win_diagonal?(:X) != false
+    raise "win_diagonal method was not successful"
+  end
+  puts "passed win_diagonal false - GREAT"
+  board.place_mark([2, 0], :X)
+  # board.print
+  if board.win_diagonal?(:X) != true
+    raise "win_diagonal method was not successful"
+  end
+
+  puts "win_diagonal method works for diagonal 2 GREAT"
+rescue
+  puts "win_diagonal method was not successful, FAIL"
+end
+
+# win?
+
+
+begin
+  board = Board.new()
+  board.place_mark([0, 0], :X)
+  board.place_mark([0, 2], :X)
+  board.place_mark([1, 1], :X)
+  # board.print
+  if board.win?(:X) != false
+    raise "win? method was not successful"
+  end
+  puts "passed win? false - GREAT"
+  board.place_mark([2, 0], :X)
+  # board.print
+  if board.win?(:X) != true
+    raise "win? method was not successful"
+  end
+
+  puts "win? method works for diagonal 2 GREAT"
+rescue
+  puts "win? method was not successful, FAIL"
+end
+
+# empty_positions?
+
+begin
+  board = Board.new()
+  board.place_mark([0, 0], :X)
+  board.place_mark([0, 1], :X)
+  board.place_mark([0, 2], :X)
+  # board.print
+  if board.empty_positions? != true
+    raise "empty_positions? method was not successful"
+  end
+  puts "passed empty_positions? true - GREAT"
+  board.place_mark([1, 0], :X)
+  board.place_mark([1, 1], :X)
+  board.place_mark([1, 2], :X)
+  board.place_mark([2, 0], :X)
+  board.place_mark([2, 1], :X)
+
+  # board.print
+  if board.empty_positions? != true
+    raise "empty_positions? method was not successful"
+  end
+  board.place_mark([2, 2], :X)
+
+  # board.print
+  if board.empty_positions? != false
+    raise "empty_positions? method was not successful"
+  end
+  puts "empty_positions? method works GREAT"
+rescue
+  puts "empty_positions? method was not successful, FAIL"
+end
